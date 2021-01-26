d72842335b6c8679ac368a41df1f06222012faca
Sergey Remizov <r@remixoff.ru>
2021-01-27 00:46:06 +0300

    Add DHT22 support

--- dht.c
+++ dht.c
@@ -28,17 +28,23 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "dht11.h"
+#include "dht.h"
+
+typedef enum{
+	MODE_DHT11,
+	MODE_DHT22
+}dht_mode_t;
 
 static gpio_num_t dht_gpio;
+static dht_mode_t dht_mode;
 static int64_t last_read_time = -2000000;
-static struct dht11_reading last_read;
+static struct dht_reading last_read;
 
 static int _waitOrTimeout(uint16_t microSeconds, int level) {
     int micros_ticks = 0;
     while(gpio_get_level(dht_gpio) == level) { 
         if(micros_ticks++ > microSeconds) 
-            return DHT11_TIMEOUT_ERROR;
+            return DHT_TIMEOUT_ERROR;
         ets_delay_us(1);
     }
     return micros_ticks;
@@ -46,9 +52,9 @@ static int _waitOrTimeout(uint16_t microSeconds, int level) {
 
 static int _checkCRC(uint8_t data[]) {
     if(data[4] == (data[0] + data[1] + data[2] + data[3]))
-        return DHT11_OK;
+        return DHT_OK;
     else
-        return DHT11_CRC_ERROR;
+        return DHT_CRC_ERROR;
 }
 
 static void _sendStartSignal() {
@@ -62,34 +68,44 @@ static void _sendStartSignal() {
 
 static int _checkResponse() {
     /* Wait for next step ~80us*/
-    if(_waitOrTimeout(80, 0) == DHT11_TIMEOUT_ERROR)
-        return DHT11_TIMEOUT_ERROR;
+    if(_waitOrTimeout(80, 0) == DHT_TIMEOUT_ERROR)
+        return DHT_TIMEOUT_ERROR;
 
     /* Wait for next step ~80us*/
-    if(_waitOrTimeout(80, 1) == DHT11_TIMEOUT_ERROR) 
-        return DHT11_TIMEOUT_ERROR;
+    if(_waitOrTimeout(80, 1) == DHT_TIMEOUT_ERROR) 
+        return DHT_TIMEOUT_ERROR;
 
-    return DHT11_OK;
+    return DHT_OK;
 }
 
-static struct dht11_reading _timeoutError() {
-    struct dht11_reading timeoutError = {DHT11_TIMEOUT_ERROR, -1, -1};
+static struct dht_reading _timeoutError() {
+    struct dht_reading timeoutError = {DHT_TIMEOUT_ERROR, -1, -1};
     return timeoutError;
 }
 
-static struct dht11_reading _crcError() {
-    struct dht11_reading crcError = {DHT11_CRC_ERROR, -1, -1};
+static struct dht_reading _crcError() {
+    struct dht_reading crcError = {DHT_CRC_ERROR, -1, -1};
     return crcError;
 }
 
-void DHT11_init(gpio_num_t gpio_num) {
+static void DHT_init(gpio_num_t gpio_num, dht_mode_t _dht_mode){
     /* Wait 1 seconds to make the device pass its initial unstable status */
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     dht_gpio = gpio_num;
+	dht_mode = _dht_mode;
 }
 
-struct dht11_reading DHT11_read() {
-    /* Tried to sense too son since last read (dht11 needs ~2 seconds to make a new read) */
+void DHT11_init(gpio_num_t gpio_num) {
+	DHT_init(gpio_num, MODE_DHT11);
+}
+
+void DHT22_init(gpio_num_t gpio_num) {
+	DHT_init(gpio_num, MODE_DHT22);
+}
+
+
+struct dht_reading DHT_read() {
+    /* Tried to sense too son since last read (dht needs ~2 seconds to make a new read) */
     if(esp_timer_get_time() - 2000000 < last_read_time) {
         return last_read;
     }
@@ -100,13 +116,13 @@ struct dht11_reading DHT11_read() {
 
     _sendStartSignal();
 
-    if(_checkResponse() == DHT11_TIMEOUT_ERROR)
+    if(_checkResponse() == DHT_TIMEOUT_ERROR)
         return last_read = _timeoutError();
     
     /* Read response */
     for(int i = 0; i < 40; i++) {
         /* Initial data */
-        if(_waitOrTimeout(50, 0) == DHT11_TIMEOUT_ERROR)
+        if(_waitOrTimeout(50, 0) == DHT_TIMEOUT_ERROR)
             return last_read = _timeoutError();
                 
         if(_waitOrTimeout(70, 1) > 28) {
@@ -115,10 +131,16 @@ struct dht11_reading DHT11_read() {
         }
     }
 
-    if(_checkCRC(data) != DHT11_CRC_ERROR) {
-        last_read.status = DHT11_OK;
-        last_read.temperature = data[2];
-        last_read.humidity = data[0];
+    if(_checkCRC(data) != DHT_CRC_ERROR) {
+        last_read.status = DHT_OK;
+		if (dht_mode == MODE_DHT11){
+	        last_read.temperature = data[2];
+		    last_read.humidity = data[0];
+		} else if (dht_mode == MODE_DHT22 ) {
+			last_read.temperature = ((data[2]<<8) + data[3])/10;
+			last_read.humidity = ((data[0]<<8) + data[1])/10;
+		} else {
+		};
         return last_read;
     } else {
         return last_read = _crcError();
